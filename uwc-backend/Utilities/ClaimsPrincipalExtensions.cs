653bcdcb6f7424888fe8a3bf77cfcd245737505e
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-07-04 21:01:23 +0700

    feat: ClaimsPrincipalHelper

---
+++ uwc-backend/Utilities/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+
+namespace Utilities;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetLoggedInUserId(this ClaimsPrincipal principal)
+    {
+        if (principal == null) throw new ArgumentNullException(nameof(principal));
+
+        var loggedInUserId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (loggedInUserId != null) return int.Parse(loggedInUserId);
+
+        throw new Exception("Invalid type provided");
+    }
+
+    public static string GetLoggedInUserName(this ClaimsPrincipal principal)
+    {
+        if (principal == null)
+            throw new ArgumentNullException(nameof(principal));
+
+        return principal.FindFirstValue(ClaimTypes.Name);
+    }
+
+    public static string GetLoggedInUserEmail(this ClaimsPrincipal principal)
+    {
+        if (principal == null)
+            throw new ArgumentNullException(nameof(principal));
+
+        return principal.FindFirstValue(ClaimTypes.Email);
+    }
+}
